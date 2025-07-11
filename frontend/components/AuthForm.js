8915e09bd5b65050d50d2a447acadb5fa782d69b
Wulnan Shadrach <wulnanshadrach268@gmail.com>
2025-07-11 02:06:59 +0100

    Authorization header with token functional

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export default function AuthForm() {
+  const navigate = useNavigate()
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value)
@@ -11,13 +16,33 @@ export default function AuthForm() {
     setPassword(event.target.value)
   }
   const handleSubmit = async (event) => {
+    event.preventDefault() //prevent default behaviour
+     setError('')         //clear any old errors and get the try catch going
+     try {
+        const {data} = await axios.post( //argument passed: endpoint path & credentials, username and password obtained from component state. 
+          '/api/auth/login',
+          {username, password}//because we're using axios, we don't need to turn these to actual JSON
+        )
+        localStorage.setItem('token', data.token)//after retrieving the data, call local storage, set key'token' and the value which comes from data.token
+        navigate('/cereals')
+     } catch (err) {
+      setError(
+      err?.response?.data?.message ||
+      'An error occurred. Please try again '
+    )
+     }
+    //post username and password to the login endpoint 
+    //if fail, set error message in state so that it renders to the jsx
+  //if ok,
+  // 1. save token to local storage
+  // 2. redirect the user to the cereals route 
 
   }
 
   return (
     <div className="container">
       <div aria-live="polite"></div>
-      <div aria-live="assertive" style={{ color: 'red' }}></div>
+      <div aria-live="assertive" style={{ color: 'red' }}>{error}</div>
       <h3>Login Form</h3>
       <form onSubmit={handleSubmit}>
         <div>
--- frontend/components/Cereals.js
+++ frontend/components/Cereals.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 
 export default function Cereals() {
   const [cereals, setCereals] = useState([])
+  const navigate = useNavigate()
 
   const logout = () => {
-
+   //wipe the token from local storage
+   localStorage.removeItem('token')
+   //redirecting the user back to login. How do we keep the user in the login screen after logging out? we'll need an effect that fires after first render.
+   navigate('/')
   }
+
+  useEffect(() => {
+    //grab token from local storage
+    const token = localStorage.getItem('token')
+    //if not there navigate user back to login
+    if(!token) {
+      navigate('/')
+    } else {
+      const fetchCereals = async () => {
+       try {
+         const response = await axios.get(
+          '/api/cereals',
+          {headers:{Authorization:token}}
+        )
+        setCereals(response.data)//wrap up the happy path by setting response.data inside component state
+       } catch (error){  //Now if the promise rejects because the response comes with a 401 unAuthorized, specifically we want to flush the stale token from local storage and kick the user back to the auth screen.
+        if (error?.response?.status == 401) logout() //logging out don't require any request to the server, it's like a frontend operation only
+        }
+        //GET cereals, appending token to authorization header
+        //if response is a 401 unauthorized perform a logout
+        //if response is ok set the in component state so that they render 
+      }
+      fetchCereals()
+    }
+  }, []) 
 
   return (
     <div className="container">
